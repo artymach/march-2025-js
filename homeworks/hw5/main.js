7e50a013441048255710a74d1a68a24a879b281c
artymach <artemmachikhin1983@gmail.com>
2025-03-31 15:08:34 +0300

    hw5

---
+++ homeworks/hw5/main.js
@@ -0,0 +1,17 @@
+// * Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
+// Приклад exchange(10000,[{currency:’USD’,value:25},{currency:’EUR’,value:42}],’USD’) // => 400
+// Зробити завдання за допомогою стрілочної функції
+
+exchange = (sumUAH, currencyValues, exchangeCurrency) => {
+    let someCurrency;
+    for (const item of currencyValues) {
+        if (item.currency === exchangeCurrency) {
+            someCurrency = item;
+        }
+    }
+    return sumUAH / someCurrency.value
+}
+
+console.log(exchange(10000, [{currency: 'USD', value: 25}, {currency: 'EUR', value: 42}], 'USD'));
+console.log(exchange(10000, [{currency: 'USD', value: 25}, {currency: 'EUR', value: 42}], 'EUR'));
+
